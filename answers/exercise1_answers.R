d6f3ff7cdace773523b22706b2ea40f9949162fd
Kumar Ramanathan <kumar@kumars-mac-mini.lan>
2021-05-18 08:33:39 -0500

    Initial commit with Rproj file, draft of slides Rmd file, and draft of exercises file

---
+++ answers/exercise1_answers.R
@@ -0,0 +1,10 @@
+#### Load libraries ####
+
+# Load tidyverse - note the output lets you know this contains ggplot2
+library(tidyverse)
+
+#### Basic plot ####
+
+#### Mulitple geoms ####
+
+#### Choose your own adventure! ####
